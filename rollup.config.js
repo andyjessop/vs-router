41e64dd7b7ed0bf7ee9683811c50c7962259bb55
Andy Jessop <andrewdjessop@gmail.com>
2020-10-20 12:12:18 +0200

    Add dist file

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import babel from '@rollup/plugin-babel';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: 'src/router/create-router.ts',
+  output: {
+    file: 'dist/router.js',
+    format: 'esm'
+  },
+  plugins: [
+    typescript(),
+    babel({ babelHelpers: 'bundled' }),
+    terser(),
+  ],
+};
